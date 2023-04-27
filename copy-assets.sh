6048cd3f801a63c9d7c49357b5f1aecf4b5f8682
Robin Berjon <robin.berjon@nytimes.com>
2023-04-27 15:31:00 -0400

    bg + title

--- copy-assets.sh
+++ copy-assets.sh
@@ -1,5 +1,4 @@
 
-# WILL LIKELY NEED THIS LATER
 cp -R node_modules/\@shoelace-style/shoelace/dist/assets ui/assets
 mkdir -p ui/css
 cp -R node_modules/\@shoelace-style/shoelace/dist/themes/light.css ui/css/
