3a039d14a045d95a3c37ba43eddbdc4d8469dd66
Robin Berjon <robin.berjon@nytimes.com>
2023-05-01 17:29:38 -0400

    'about, because fuck yeah this has to ui'

--- ui/el/tile.js
+++ ui/el/tile.js
@@ -13,8 +13,19 @@ export class CosmoTile extends LitElement {
         width: 100%;
         min-height: 450px;
       }
+      h3 {
+        margin: 0;
+      }
       h3 img {
         vertical-align: middle;
+      }
+      [slot='header'] {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      }
+      sl-dialog.about::part(body) {
+        text-align: center;
       }
     `
   ];
@@ -36,11 +47,15 @@ export class CosmoTile extends LitElement {
     this.meta = await res.json();
     console.warn(`meta`, this.meta);
   }
+  handleMenu (ev) {
+    const menu = ev.detail?.item?.value;
+    if (menu === 'about') {
+      this.shadowRoot.querySelector('sl-dialog.about').show();
+    }
+  }
   render () {
     if (!this.src) return nothing;
     // XXX
-    //  - make this a cm-tile element
-    //  - add a ••• drop down
     //  - about menu
     //  - footer
     //  - webview params
@@ -48,17 +63,34 @@ export class CosmoTile extends LitElement {
     //  - autoresize based on content
     //  - layout to be flush
     //  - notes on a better monitor somewhere, too
+    const icon = this.meta.icons?.[0]?.src ? new URL(this.meta.icons[0].src, this.src).href : null;
+    const name = this.meta.name || this.meta.short_name || 'Untitled';
     return html`
       <sl-card>
         <div slot="header">
           <h3>
-            ${this.meta.icons?.[0]?.src
-              ? html`<img src=${new URL(this.meta.icons[0].src, this.src).href} width="32" height="32" class='tile-icon'>`
+            ${icon
+              ? html`<img src=${icon} width="32" height="32" class='tile-icon'>`
               : nothing}
-              ${this.meta.name || this.meta.short_name || 'Untitled'}
+            ${name}
           </h3>
+          <sl-dropdown hoist>
+            <sl-icon-button name="three-dots-vertical" label="Actions" slot="trigger"></sl-icon-button>
+            <sl-menu @sl-select=${this.handleMenu}>
+              <sl-menu-item value='about'>
+                ${icon ? html`<img src=${icon} width="32" height="32" class='tile-icon' slot="prefix">` : nothing}
+                About ${name}
+              </sl-menu-item>
+            </sl-menu>
+          </sl-dropdown>
         </div>
         <webview src=${this.src}></webview>
+        <sl-dialog label="About" class="about">
+          ${icon
+            ? html`<img src=${icon} width="64" height="64"><br>`
+            : nothing}
+          <strong>${name}</strong>
+        </sl-dialog>
       </sl-card>
     `;
   }
